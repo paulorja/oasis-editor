f0b3f6c3b11b73c80151b646029f7b15a8d03e7d
PAULO JANSEN <pauloo.jansen@gmail.com>
2017-07-15 17:39:02 -0300

    afsd

---
+++ unit.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="unit" tilewidth="64" tileheight="64" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../../desenvolvimento/potato-games/oasis-client/static/sprites/world/unit/tree3.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../../desenvolvimento/potato-games/oasis-client/static/sprites/world/unit/tree2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../../desenvolvimento/potato-games/oasis-client/static/sprites/world/unit/tree.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="../../desenvolvimento/potato-games/oasis-client/static/sprites/world/unit/stone.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="../../desenvolvimento/potato-games/oasis-client/static/sprites/world/unit/chest.png"/>
+ </tile>
+</tileset>
