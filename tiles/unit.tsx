949cb3779ec5c544d9b613f50d980c465624574f
PAULO JANSEN <pauloo.jansen@gmail.com>
2017-07-22 01:46:24 -0300

    fence working

--- tiles/unit.tsx
+++ tiles/unit.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="unit" tilewidth="64" tileheight="64" tilecount="8" columns="0">
+<tileset name="unit" tilewidth="64" tileheight="64" tilecount="9" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <terraintypes>
   <terrain name="" tile="0"/>
@@ -27,5 +27,8 @@
  </tile>
  <tile id="8">
   <image width="64" height="64" source="../sprites/unit/pink_wall.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="../sprites/unit/fence.png"/>
  </tile>
 </tileset>
