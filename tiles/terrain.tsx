51db212af0d0ccd4e7f53ad28acbac6077fcbbe7
PAULO JANSEN <pauloo.jansen@gmail.com>
2017-07-24 01:50:30 -0300

    climb

--- tiles/terrain.tsx
+++ tiles/terrain.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="grass" tilewidth="64" tileheight="64" tilecount="4" columns="0">
+<tileset name="grass" tilewidth="64" tileheight="64" tilecount="5" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="64" height="64" source="../../oasis-client/development/sprites/world/terrain/grass.png"/>
@@ -12,5 +12,8 @@
  </tile>
  <tile id="3">
   <image width="64" height="64" source="../sprites/terrain/water_sand.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="../sprites/terrain/climb.png"/>
  </tile>
 </tileset>
