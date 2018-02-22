c790eff86b6f0cfcea728353db6431d1ef006bff
PAULO JANSEN <pauloo.jansen@gmail.com>
2018-02-21 22:41:10 -0300

    fixing object paths

--- tiles/terrain.tsx
+++ tiles/terrain.tsx
@@ -2,7 +2,7 @@
 <tileset name="grass" tilewidth="64" tileheight="64" tilecount="16" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="64" height="64" source="../../oasis-client/development/sprites/world/terrain/grass.png"/>
+  <image width="64" height="64" source="../sprites/terrain/grass.png"/>
  </tile>
  <tile id="1">
   <image width="64" height="64" source="../sprites/terrain/water.png"/>
