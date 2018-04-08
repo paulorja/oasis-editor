87271d452a7cd4e9c73df3c81dae726feefcaa71
PAULO JANSEN <pauloo.jansen@gmail.com>
2018-04-08 15:02:24 -0300

    npcs

---
+++ tiles/npc.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="npc" tilewidth="64" tileheight="64" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../sprites/npc/1_npc.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../sprites/npc/2_npc.png"/>
+ </tile>
+</tileset>
